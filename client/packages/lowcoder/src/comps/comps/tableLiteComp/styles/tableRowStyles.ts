9adfa1307a7c92c38cd9af354f4b4c5aebb53a48
FARAN <faran1997@outlook.com>
2025-08-21 21:28:36 +0500

    setup structure for the styles

---
+++ client/packages/lowcoder/src/comps/comps/tableLiteComp/styles/tableRowStyles.ts
@@ -0,0 +1,10 @@
+import { css } from "styled-components";
+import { TableRowStyleType } from "comps/controls/styleControlConstants";
+
+export const getTableRowStyles = (
+  rowStyle: TableRowStyleType
+) => css`
+  /* TODO: Implement row styles here */
+  /* You can follow the pattern from main table's getStyle function */
+  /* This should include: background, alternateBackground, selectedRowBackground, hoverRowBackground, etc. */
+`; 
---
+++ client/packages/lowcoder/src/comps/comps/tableLiteComp/styles/tableToolbarStyles.ts
@@ -0,0 +1,9 @@
+import { css } from "styled-components";
+import { TableToolbarStyleType } from "comps/controls/styleControlConstants";
+
+export const getTableToolbarStyles = (
+  toolbarStyle: TableToolbarStyleType
+) => css`
+  /* TODO: Implement toolbar styles here */
+  /* This should include toolbar background, text, borders, etc. */
+`; 
